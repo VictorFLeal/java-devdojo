67305a4e1ccc2544d5213b42b9a5dee731bc039b
Victor Leal <victorlealdev@gmail.com>
2026-05-27 16:26:34 -0400

    Comecei Estrutura de Repetição aula01

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        // while, do while, for
+        int count = 0;
+        while (true){
+
+        }
+    }
+}
